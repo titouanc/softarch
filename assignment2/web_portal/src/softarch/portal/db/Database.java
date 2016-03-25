2d849c59a3c28ff55fba1d74c484f4c22dde97cd
t00n <kelkununtel@hotmail.com>
2016-03-25 13:22:23 +0100

    [fix] abstract database

--- assignment2/web_portal/src/softarch/portal/db/Database.java
+++ assignment2/web_portal/src/softarch/portal/db/Database.java
@@ -1,11 +1,13 @@
 package softarch.portal.db;
 
+import java.util.List;
+
 import softarch.portal.data.Data;
 
 public interface Database {
 	public abstract void create(Data data);
 	
-	public abstract Data retrieve(int id);
+	public abstract List<Data> retrieve(String query);
 	
 	public abstract void update(Data data);
 	
